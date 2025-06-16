1a5ad52f456156c80df010f4669c9a4d58012e8b
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2025-06-16 12:45:20 +0700

    refactor: Add image slider content to project data for enhanced visual representation

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,4 @@
-import { Project } from "../types/index";
+import { Project, ImageSliderContent } from "../types/index";
 
 const projectsData: Project[] = [
   {
@@ -9,6 +9,40 @@ const projectsData: Project[] = [
     status: "Completed",
     github: "https://github.com/orgs/PPL-BRIN/repositories",
     demo: "https://pantautular.netlify.app",
+    images: [
+      {
+        src: "/pantautular/PantauTular - About.png",
+        alt: "/pantautular/PantauTular - About"
+      },
+      {
+        src: "/pantautular/PantauTular - Bantuan.png",
+        alt: "PantauTular - Bantuan"
+      },
+      {
+        src: "/pantautular/PantauTular - Register.png",
+        alt: "PantauTular - Register"
+      },
+      {
+        src: "/pantautular/PantauTular - Landing Page.png",
+        alt: "PantauTular - Landing Page"
+      },
+      {
+        src: "/pantautular/PantauTular - Peta Tematik.png",
+        alt: "PantauTular - Peta Tematik"
+      },
+      {
+        src: "/pantautular/PantauTular - Case Level.png",
+        alt: "PantauTular - Case Level"
+      },
+      {
+        src: "/pantautular/PantauTular - Demografi.png",
+        alt: "PantauTular - Demografi"
+      },
+      {
+        src: "/pantautular/PantauTular - Snapshot.png",
+        alt: "PantauTular - Snapshot"
+      },
+    ] as ImageSliderContent[]
   },
   {
     id: 2,
@@ -16,7 +50,21 @@ const projectsData: Project[] = [
     description: "Manuscript search engine application for Indonesian researchers, providing advanced search features and a user-friendly interface. Built using Scrapy as the web scraping framework and Elasticsearch for indexing and searching manuscripts.",
     technologies: ["Scrapy", "Elasticsearch"],
     status: "Completed",
-    demo: "https://www.temanusa.com/id"
+    demo: "https://www.temanusa.com/id",
+    images: [
+      {
+        src: "/temanusa/Temanusa - Search Bar.png",
+        alt: "Temanusa - Search Bar"
+      },
+      {
+        src: "/temanusa/Temanusa - Search Section.png",
+        alt: "Temanusa - Search Section"
+      },
+      {
+        src: "/temanusa/Temanusa - The Team.png",
+        alt: "Temanusa - The Team"
+      }
+    ] as ImageSliderContent[]
   },
   {
     id: 3,
@@ -25,6 +73,20 @@ const projectsData: Project[] = [
     technologies: ["Vue.js", "Fast API"],
     status: "Completed",
     github: "https://github.com/ghanahmada/finsearch-be",
+    images: [
+      {
+        src: "/pantautular/PantauTular - .jpg",
+        alt: "PantauTular - "
+      },
+      {
+        src: "/pantautular/PantauTular - .jpg",
+        alt: "PantauTular - "
+      },
+      {
+        src: "/pantautular/PantauTular - .jpg",
+        alt: "PantauTular - "
+      }
+    ] as ImageSliderContent[]
   },
   {
     id: 4,
@@ -32,7 +94,21 @@ const projectsData: Project[] = [
     description: "A music catalogue application that allows users to search and discover music tracks. Developed using Django and PostgreSQL, it features a raw SQL database for storing music metadata. Developed as a final project for the Database course.",
     technologies: ["PostgreSQL", "Django"],
     status: "Completed",
-    github: "https://github.com/zuhdynadhif/F11_Basdat"
+    github: "https://github.com/zuhdynadhif/F11_Basdat",
+    images: [
+      {
+        src: "/pantautular/PantauTular - .jpg",
+        alt: "PantauTular - "
+      },
+      {
+        src: "/pantautular/PantauTular - .jpg",
+        alt: "PantauTular - "
+      },
+      {
+        src: "/pantautular/PantauTular - .jpg",
+        alt: "PantauTular - "
+      }
+    ] as ImageSliderContent[]
   },
   {
     id: 5,
@@ -40,7 +116,21 @@ const projectsData: Project[] = [
     description: "An e-commerce platform for buying and selling toys, featuring product listings. Developed as a final project for the Advanced Programming course, it includes a web interface for users to browse and purchase toys.",
     technologies: ["Django", "PostgreSQL", "Microservices", "RESTful API"],
     status: "Completed",
-    github: "https://github.com/orgs/advpro-b3/repositories"
+    github: "https://github.com/orgs/advpro-b3/repositories",
+    images: [
+      {
+        src: "/pantautular/PantauTular - .jpg",
+        alt: "PantauTular - "
+      },
+      {
+        src: "/pantautular/PantauTular - .jpg",
+        alt: "PantauTular - "
+      },
+      {
+        src: "/pantautular/PantauTular - .jpg",
+        alt: "PantauTular - "
+      }
+    ] as ImageSliderContent[]
   },
   {
     id: 6,
@@ -48,7 +138,21 @@ const projectsData: Project[] = [
     description: "A bookstore application that allows users to browse and borrow books. Developed as a final project for the Platform-Based Programming course, it features a web and mobile interface for users to search and borrow books.",
     technologies: ["Flutter", "Django", "PostgreSQL"],
     status: "Completed",
-    github: "https://github.com/ReadNow-C14"
+    github: "https://github.com/ReadNow-C14",
+    images: [
+      {
+        src: "/pantautular/PantauTular - .jpg",
+        alt: "PantauTular - "
+      },
+      {
+        src: "/pantautular/PantauTular - .jpg",
+        alt: "PantauTular - "
+      },
+      {
+        src: "/pantautular/PantauTular - .jpg",
+        alt: "PantauTular - "
+      }
+    ] as ImageSliderContent[]
   }
 ];
 
